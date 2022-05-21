6f1b39244bbdfedc994cf1d81bd41528ec9adcf3
walid-epc <micro.epc@gmail.com>
2022-05-22 01:15:55 +0200

    commit-msg

---
+++ build/app/generated/source/buildConfig/debug/com/popular/popular_people_cloud/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.popular.popular_people_cloud;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.popular.popular_people_cloud";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
